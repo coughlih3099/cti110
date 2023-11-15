db355584e2aa7b0a342aa224788987e76422a7c7
coughlih3099 <145471617+coughlih3099@users.noreply.github.com>
2023-11-15 16:31:24 -0500

    P5LAB - Leap year function
    Extension of P3LAB that runs leap year checking as a function returning the amount of days in February for a given year.

---
+++ P5LAB_CoughlinHarley.py
@@ -0,0 +1,21 @@
+# this program checks whether a year is a leap year and then returns the amount
+# of days in February.
+# 11/15/2023
+# CTI-110 P5LAB - days in feb
+# Harley Coughlin
+#
+
+def days_in_feb(user_year):
+ if (user_year % 100 == 0):
+    if (user_year % 400 == 0):
+         return 29
+    else:
+        return 28
+ elif (user_year % 4 == 0):
+    return 29
+ else:
+    return 28
+
+if __name__ == '__main__':
+    user_year = int(input("Enter a year: "))
+    print(f"{user_year} has {days_in_feb(user_year)} days in February.")
