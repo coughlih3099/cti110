8a6b2af722bccad5fd5c9ff9e02eaf4d67adf166
coughlih3099 <145471617+coughlih3099@users.noreply.github.com>
2023-12-01 11:45:06 -0500

    Create sub.py
    subtraction function for math quiz

---
+++ P5HW_CoughlinHarley/sub.py
@@ -0,0 +1,24 @@
+import random
+import check
+
+def subtraction():
+
+  num1 = random.randint(1, 300)
+  num2 = random.randint(1, 300)
+  subtracted = num1 - num2
+
+  print(f"{num1:>5}\n- {num2:>3}\n\n")
+  
+  num_guesses = 0
+  retry = True
+  while (retry):
+    if num_guesses == 0:
+      user_input = int(input("Enter answer: "))
+    else:
+      user_input = int(input("Try again: "))
+
+    num_guesses += 1
+    retry = check(user_input, subtracted)
+    continue
+  
+  print(f"Number of guesses: {num_guesses}\n")
