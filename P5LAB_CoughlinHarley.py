cd1458b676202392378710a7151bc1cd97e07ad6
coughlih3099 <145471617+coughlih3099@users.noreply.github.com>
2023-12-01 12:10:04 -0500

    Update P5LAB_CoughlinHarley.py
    changed to while loop

--- P5LAB_CoughlinHarley.py
+++ P5LAB_CoughlinHarley.py
@@ -6,16 +6,19 @@
 #
 
 def days_in_feb(user_year):
- if (user_year % 100 == 0):
-    if (user_year % 400 == 0):
-         return 29
+    if (user_year % 100 == 0):
+        if (user_year % 400 == 0):
+            return 29
+        else:
+            return 28
+    elif (user_year % 4 == 0):
+        return 29
     else:
         return 28
- elif (user_year % 4 == 0):
-    return 29
- else:
-    return 28
 
-if __name__ == '__main__':
+keep_going = 'y'
+while keep_going != 'n':
     user_year = int(input("Enter a year: "))
     print(f"{user_year} has {days_in_feb(user_year)} days in February.")
+    keep_going = input("Do you want to enter another year? (y or n)")
+    continue
